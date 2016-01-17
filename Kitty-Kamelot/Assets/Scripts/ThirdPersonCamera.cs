92a9faddef9352cdd1474c478625bcb136f54eaf
kkalina <kkalina@umich.edu>
2016-01-17 09:50:15 -0500

    init

---
+++ Kitty-Kamelot/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+	public int playerCamNumber;
+	public int numPlayers;
+
+	public Transform poi;
+	public Transform camTarget;
+	public float distance = 5, height = 1;
+	public float u = 0.1f;
+	//public float v = .1f;
+
+	public bool shake = false;
+	public float shakeIntensity = 0.1f;
+
+	void FixedUpdate()
+	{
+		Vector3 pos = poi.position;
+		pos -= poi.forward * distance;
+		pos += poi.up * height;
+		//Vector3 rot = poi.localEulerAngles;
+
+		Vector3 pos2 = (1 - u) * transform.position + u * pos;
+		//Vector3 rot2 = (1 - v) * transform.localEulerAngles + v * rot;
+
+		if (shake) {
+			pos2.x += Random.value * shakeIntensity * Random .Range(-1, 1);
+			pos2.y += Random.value * shakeIntensity * Random .Range(-1, 1);
+			pos2.z += Random.value * shakeIntensity * Random .Range(-1, 1);
+		}
+
+		transform.position = pos2;
+
+		transform.LookAt(camTarget);
+		//transform.localEulerAngles = rot2;
+
+	}
+}
---
+++ Kitty-Kamelot/Assets/playerSpawner.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerSpawner : MonoBehaviour {
+    public bool p1active = false;
+    public bool p2active = false;
+    public bool p3active = false;
+    public bool p4active = false;
+
+    public GameObject playerPrefab;
+
+    public float playerSpacing = 2f;
+
+    // Use this for initialization
+    void Start () {
+
+        //Testing purposes
+        p1active = true;
+        p2active = true;
+        p3active = true;
+        p4active = true;
+
+        if (p1active)
+        {
+            GameObject player1 = Instantiate(playerPrefab);
+            player1.GetComponent<ShipControl>().playerIndexNum = XInputDotNetPure.PlayerIndex.One;
+            player1.transform.position = new Vector3(this.transform.position.x + (playerSpacing*1),this.transform.position.y,this.transform.position.z);
+        }
+        if (p2active)
+        {
+            GameObject player2 = Instantiate(playerPrefab);
+            player2.GetComponent<ShipControl>().playerIndexNum = XInputDotNetPure.PlayerIndex.Two;
+            player2.transform.position = new Vector3(this.transform.position.x + (playerSpacing * 2), this.transform.position.y, this.transform.position.z);
+        }
+        if (p3active)
+        {
+            GameObject player3 = Instantiate(playerPrefab);
+            player3.GetComponent<ShipControl>().playerIndexNum = XInputDotNetPure.PlayerIndex.Three;
+            player3.transform.position = new Vector3(this.transform.position.x + (playerSpacing * 3), this.transform.position.y, this.transform.position.z);
+        }
+        if (p4active)
+        {
+            GameObject player4 = Instantiate(playerPrefab);
+            player4.GetComponent<ShipControl>().playerIndexNum = XInputDotNetPure.PlayerIndex.Four;
+            player4.transform.position = new Vector3(this.transform.position.x + (playerSpacing * 4), this.transform.position.y, this.transform.position.z);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
